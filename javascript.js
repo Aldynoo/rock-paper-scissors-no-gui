fffeef56539d0e18ef3a10529c1aa73fb9d710d6
Aldynoo <aldinidrizoski9@gmail.com>
2026-03-24 16:35:05 +0100

    Added playRound function and some minor tweaks

--- javascript.js
+++ javascript.js
@@ -1,20 +1,59 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
 function getComputerChoice () {
     let answer = Math.random();
         if (answer <= 0.33) {
-            console.log("Rock")
+            return "rock"
         }
         else if (answer <= 0.67) {
-            console.log("Paper")
+            return "paper"
         }
         else {
-            console.log("Scissors")
+            return "scissors"
         }
-    return answer;
 }
 console.log(getComputerChoice());
 
+
 function getHumanChoice() {
     let myChoice = prompt('Enter "Rock", "Paper" or "Scissors"')
-    return myChoice;
+    return myChoice.toLowerCase();
 }
-console.log(getHumanChoice());
+//console.log(getHumanChoice());
+
+
+function playRound (humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (computerChoice === humanChoice) {
+        console.log("It's a tie!")
+    }
+    else if (computerChoice === "rock" && humanChoice === "paper") {
+        console.log("You win! Paper beats rock!")
+    }
+    else if (computerChoice === "paper" && humanChoice === "rock") {
+        console.log("You lose! Paper beats rock!")
+    }
+    else if (computerChoice === "scissors" && humanChoice === "paper") {
+        console.log("You lose! Scissors beats paper!")
+    }
+    else if (computerChoice === "paper" && humanChoice === "scissors") {
+        console.log("You win! Scissors beats paper!")
+    }
+    else if (computerChoice === "rock" && humanChoice === "scissors") {
+        console.log("You lose! Rock beats scissors!")
+    }
+    else if (computerChoice === "scissors" && humanChoice === "rock") {
+        console.log("You win! Rock beats scissors!")
+    } 
+    else {
+        console.log("Invalid input!")
+    }
+}
+console.log(playRound(humanChoice, computerChoice));
+
+
